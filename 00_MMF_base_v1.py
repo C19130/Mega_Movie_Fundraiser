a0c6cdbc2a0226d59767f57a876c464ab2cef043
Cade Young <82617358+C19130@users.noreply.github.com>
2022-06-08 22:56:08 +0000

    added comments

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,31 @@
+#important staements
+
+
+#functions go here
+
+
+# ********** Main Routine **********
+
+# Set up dictionaries / Lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket details 
+
+  # Get name (can't be blank)
+
+  # Get age (between 12 and 130)
+
+  # Calculate  ticket price:
+
+  # Loop to ask for snacks
+
+  # Calculate snack price
+
+  # Ask for payment method (and apply surcharge if necesary) 
+
+
+# Calculate Total sales and profit
+
+# Output data to text file
+
