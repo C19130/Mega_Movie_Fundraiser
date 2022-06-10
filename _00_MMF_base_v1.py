47d8fb8c5a65fe85b611c1a9eeedfcd320dea806
Cade Young <82617358+C19130@users.noreply.github.com>
2022-06-10 00:02:15 +0000

    added blank name function to main file

--- _00_MMF_base_v1.py
+++ _00_MMF_base_v1.py
@@ -1,7 +1,20 @@
-#important staements
+# important staements
 
 
-#functions go here
+# functions go here
+
+# checks that ticket name is not blank
+def not_blank(question):
+  valid = False
+
+  while not valid:
+    response = input(question)
+
+    if response != "":
+      return response 
+    else:
+      print("Sorry - This can't be blank, "
+           "please enter your name")
 
 
 # ********** Main Routine **********
